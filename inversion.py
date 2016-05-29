2c28b7b4a5e74c44fab4ac46f428f71fa58bc112
AnichinDaniil <anichindaniil@gmail.com>
2016-05-29 18:57:52 +0300

    Second week passed

--- inversion.py
+++ inversion.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-#
+from urllib2 import urlopen
 
 
 def inversion_calc(like_list):
-    # inversions = []
     inversion_number = 0
+    # inversions = []
     for i in range(len(like_list) - 1):
         for j in range(i, len(like_list)):
             if like_list[j] < like_list[i]:
-                inversion_number += 1
-    return inversion_number
                 # inversions.append((i, j))
     # return inversions
+                inversion_number += 1
+    return inversion_number
 
 
-def inversion_recurcy(like_list):
+def make_list(first_user, second_user):
+    result = [None for i in range(len(first_user))]
+    for i in range(len(first_user)):
+        result[first_user[i] - 1] = second_user[i]
+    return result
+
+
+def inversion_recursion(like_list):
+    # there are errors on the huge numbers
     half_len = len(like_list) / 2
     first = like_list[:half_len]
     second = like_list[half_len:]
     if len(like_list) > 2:
-        return inversion_recurcy(first) + inversion_recurcy(second) + \
-            split_inversions(first, second)
+        return inversion_recursion(first) + inversion_recursion(second) + \
+               split_inversions(first, second)
     else:
         return split_inversions(first, second)
 
@@ -40,6 +49,28 @@ def split_inversions(first, second):
     return inversion_number
 
 
+def main():
+    small_list = urlopen('http://courses.prometheus.org.ua/c4x/KPI/'
+                         'Algorithms101/asset/input_1000_5.txt')
+    huge_list = urlopen('http://courses.prometheus.org.ua/c4x/KPI/'
+                        'Algorithms101/asset/input_1000_100.txt')
+
+    users = []
+    for line in huge_list.readlines()[1:]:
+        numbers = line.split(' ')
+        index = int(numbers[0])
+        users.append([])
+        for number in numbers[1:]:
+            users[index - 1].append(int(number))
+
+    inversion_list = make_list(users[617], users[0])
+    print inversion_recursion(inversion_list)
+    print inversion_calc(inversion_list)
+    # print users
+    # print len(users)
+
+
 if __name__ == '__main__':
-    print inversion_calc([4, 1, 3, 2, 0])
-    print inversion_recurcy([4, 1, 3, 2, 0])
+    main()
+    # print inversion_calc([4, 1, 3, 2, 0])
+    # print inversion_recursion([4, 1, 3, 2, 0])
